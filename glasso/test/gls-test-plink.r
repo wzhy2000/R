c95d2356922ea2311b068d6c2700439700d10ed2
Zhong Wang <zw224@login-0-0.local>
2015-02-04 16:39:32 -0500

    new test files

---
+++ glasso/test/gls-test-plink.r
@@ -0,0 +1,56 @@
+library(glasso)
+
+#file.plink.bed <- "/home/zw224/f/bmi/FHS-bmi-v1.bed"  
+#file.plink.bim <- "/home/zw224/f/bmi/FHS-bmi-v1.bim"  
+#file.plink.fam <- "/home/zw224/f/bmi/FHS-bmi-v1.fam"  
+
+file.plink.bed <- "/home/zw224/f/bmi/FHS-bmi-v1-chr2.bed"  
+file.plink.bim <- "/home/zw224/f/bmi/FHS-bmi-v1-chr2.bim"  
+file.plink.fam <- "/home/zw224/f/bmi/FHS-bmi-v1-chr2.fam"  
+
+file.phe.long  <- "/home/zw224/f/bmi/bmi-phenos-sex-longtime.csv"  
+
+ret1<-ret2<-ret3<-ret4<-ret5<-ret6<-ret7<-ret8<-c();
+
+ret1 <- gls.plink( file.phe.long, file.plink.bed, file.plink.bim, file.plink.fam, Y.prefix="Y", Z.prefix="Z", covar.names=c("X"), fgwas.filter = T , options=list(nParallel.cpu=7) );	
+
+save( ret1, ret2, ret3, ret4, ret5, ret6, ret7, ret8, file="gls-test-plink.rdata");
+summary(ret1);
+plot(ret1);
+
+ret2 <- gls.plink( file.phe.long, file.plink.bed, file.plink.bim, file.plink.fam, Y.prefix="Y", Z.prefix="Z", covar.names=NULL, fgwas.filter = F );	
+
+save( ret1, ret2, ret3, ret4, ret5, ret6, ret7, ret8, file="gls-test-plink.rdata");
+summary(ret2);
+plot(ret2);
+
+ret3 <- gls.plink( file.phe.long, file.plink.bed, file.plink.bim, file.plink.fam, Y.prefix="Y", Z.prefix="Z", covar.names=c(),    fgwas.filter = T,  refit = F, options=list(nParallel.cpu=7)  );	
+
+save( ret1, ret2, ret3, ret4, ret5, ret6, ret7, ret8, file="gls-test-plink.rdata");
+summary(ret3);
+plot(ret3);
+
+ret4 <- gls.plink( file.phe.long, file.plink.bed, file.plink.bim, file.plink.fam, Y.prefix="Y", Z.prefix="Z", covar.names=c("X"), fgwas.filter = T , options=list(nParallel.cpu=7));	
+
+save( ret1, ret2, ret3, ret4, ret5, ret6, ret7, ret8, file="gls-test-plink.rdata");
+summary(ret4);
+plot(ret4);
+
+ret5 <- gls.plink( file.phe.long, file.plink.bed, file.plink.bim, file.plink.fam, Y.prefix="Y", Z.prefix="Z", covar.names=c("X"), fgwas.filter = T , add.used=T, dom.used=F , options=list(nParallel.cpu=7));	
+
+save( ret1, ret2, ret3, ret4, ret5, ret6, ret7, ret8, file="gls-test-plink.rdata");
+summary(ret5);
+plot(ret5);
+
+ret6 <- gls.plink( file.phe.long, file.plink.bed, file.plink.bim, file.plink.fam, Y.prefix="Y", Z.prefix="Z", covar.names=c("X"), fgwas.filter = T,  refit = F , options=list(nParallel.cpu=7));	
+
+save( ret1, ret2, ret3, ret4, ret5, ret6, ret7, ret8, file="gls-test-plink.rdata");
+summary(ret6);
+plot(ret6);
+
+ret7 <- gls.plink( file.phe.long, file.plink.bed, file.plink.bim, file.plink.fam, Y.prefix="Y", Z.prefix="Z", covar.names=c("X"), fgwas.filter = T,  refit=F, add.used=F, dom.used=T, options=list(nParallel.cpu=7) );
+
+save( ret1, ret2, ret3, ret4, ret5, ret6, ret7, ret8, file="gls-test-plink.rdata");
+summary(ret7);
+plot(ret7);
+
---
+++ glasso/test/gls-test-simple.r
@@ -0,0 +1,70 @@
+library(glasso)
+
+phe.out <- "gls.simple.phe"  
+snp.out <- "gls.simple.snp"
+
+a_effect <- array(0, dim=c(3,5));
+d_effect <- array(0, dim=c(3,5));
+cov_effect <- array(0, dim=c(2,4));
+
+sigsnp <- c(1:5);
+a_effect[1,]<-c( sigsnp[1], 1.04, 0.885, -2.055, 0.545);
+a_effect[2,]<-c( sigsnp[2], 1.17, -0.20, 0.74, -4.715);
+a_effect[3,]<-c( sigsnp[3], 1.40, -2.25, 1.00,  0.00);
+
+d_effect[1,]<-c( sigsnp[3], 1.49, -2.135, 4.82, 1.425);
+d_effect[2,]<-c( sigsnp[4], 1.045, 1.320, 1.905,  1.535);
+d_effect[3,]<-c( sigsnp[5], 1.265, -1.225, 2.710, -1.96);
+
+cov_effect[1,]<-c( 2.49, -1.135, 0.82, 0.425);
+cov_effect[2,]<-c( -1.045, 2.320, 0.905,  0.535);
+
+gls.simulate( phe.out, snp.out, simu_n= 300, simu_grp=1, simu_p=3000, simu_snp_rho=0.4, simu_rho=0.1, simu_sigma2= 4, 
+		 simu_mu= c(13.395, -3.08, 1.875, -3.195),  simu_covar_effect=cov_effect, simu_covar_range=c(-1,1),
+		 simu_add_effect=a_effect,  simu_dom_effect=d_effect, 
+		 simu_z_range = c(30,60), simu_z_count = c(5,12), debug=F);
+
+ret1<-ret2<-ret3<-ret4<-ret5<-ret6<-ret7<-c();
+	
+ret1 <- gls.simple(phe.out, snp.out, Y.prefix="Y", Z.prefix="Z", covar.names=c("X_1","X_2"), fgwas.filter = T );	
+
+save(ret1, ret2, ret3, ret4, ret5, ret6, file="gls-test-simple.rdata");
+summary(ret1);
+plot(ret1);
+
+ret2 <- gls.simple(phe.out, snp.out, Y.prefix="Y", Z.prefix="Z", covar.names=c("X_1","X_2"), fgwas.filter = F );	
+
+save(ret1, ret2, ret3, ret4, ret5, ret6, file="gls-test-simple.rdata");
+summary(ret2);
+plot(ret2);
+
+ret3 <- gls.simple(phe.out, snp.out, Y.prefix="Y", Z.prefix="Z", covar.names=c("X_1"), fgwas.filter = T , options=list(nParallel.cpu=7) );	
+
+save(ret1, ret2, ret3, ret4, ret5, ret6, file="gls-test-simple.rdata");
+summary(ret3);
+plot(ret3);
+
+ret4 <- gls.simple(phe.out, snp.out, Y.prefix="Y", Z.prefix="Z", covar.names=c("X_1"), fgwas.filter = T,  refit = F , options=list(nParallel.cpu=7) );	
+
+save(ret1, ret2, ret3, ret4, ret5, ret6, file="gls-test-simple.rdata");
+summary(ret4);
+plot(ret4);
+
+ret5 <- gls.simple(phe.out, snp.out, Y.prefix="Y", Z.prefix="Z", covar.names=c(), fgwas.filter = T,  refit = F, options=list(nParallel.cpu=7)  );	
+
+save(ret1, ret2, ret3, ret4, ret5, ret6, file="gls-test-simple.rdata");
+summary(ret5);
+plot(ret5);
+
+ret6 <- gls.simple(phe.out, snp.out, Y.prefix="Y", Z.prefix="Z", covar.names=c(), fgwas.filter = T,  refit = F, options=list(nParallel.cpu=7) );
+
+save(ret1, ret2, ret3, ret4, ret5, ret6, file="gls-test-simple.rdata");
+summary(ret6);
+plot(ret6);
+
+ret7 <- gls.simple(phe.out, snp.out, Y.prefix="Y", Z.prefix="Z", covar.names=c("X_1","X_2"), fgwas.filter=F,  refit=F, add.used=T, dom.used=F, options=list(nParallel.cpu=7)  );
+
+save(ret1, ret2, ret3, ret4, ret5, ret6, ret7, file="gls-test-simple.rdata");
+summary(ret7);
+plot(ret7);
+
