85ef7c502bc99613cd3ca9e51dcfca7597516139
ZHONG WANG <tg826494@login2.stampede.tacc.utexas.edu>
2015-03-18 21:50:44 -0500

    SLURM batch file

--- gwas.lasso/test/bls-test-snpmat.bat
+++ gwas.lasso/test/bls-test-snpmat.bat
@@ -4,9 +4,9 @@
 # Generic SLURM script 
 #----------------------------------------------------
 
-#SBATCH -J scan-motif # Job name
-#SBATCH -o bls.%j.out # stdout; %j expands to jobid
-#SBATCH -e bls.%j.err # stderr; skip to combine stdout and stderr
+#SBATCH -J bls.test.snpmat # Job name
+#SBATCH -o bls.test.snpmat.%j.out # stdout; %j expands to jobid
+#SBATCH -e bls.test.snpmat.%j.err # stderr; skip to combine stdout and stderr
 #SBATCH -p normal # queue
 #SBATCH -N 1 -n 8 # one node and one task
 #SBATCH -t 48:00:00 # max time
--- gwas.lasso/test/gls-test-simple.bat
+++ gwas.lasso/test/gls-test-simple.bat
@@ -4,9 +4,9 @@
 # Generic SLURM script 
 #----------------------------------------------------
 
-#SBATCH -J scan-motif # Job name
-#SBATCH -o gls.%j.out # stdout; %j expands to jobid
-#SBATCH -e gls.%j.err # stderr; skip to combine stdout and stderr
+#SBATCH -J gls.test.simple # Job name
+#SBATCH -o gls.test.simple.%j.out # stdout; %j expands to jobid
+#SBATCH -e gls.test.simple.%j.err # stderr; skip to combine stdout and stderr
 #SBATCH -p normal # queue
 #SBATCH -N 1 -n 8 # one node and one task
 #SBATCH -t 48:00:00 # max time
