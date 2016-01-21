058c93398d526e09fae0021e01208dfc21ddc546
ZHONG WANG <tg826494@login1.stampede.tacc.utexas.edu>
2016-01-20 21:31:21 -0600

    package installation

--- Funmap2/R/fm2.api.r
+++ Funmap2/R/fm2.api.r
@@ -36,13 +36,23 @@
 ###############################################################
 
 # for rmultnorm function, MSBVAR is necessary.
-#library(MSBVAR);
+library(MSBVAR);
+
+FM_sys <- NULL;
 
 FM2.start <- function()
 {
 	FM_sys <<- FM2.sys("FM");
 	FM_sys$reset();
 }
+
+FM2.curves   <- list();
+FM2.covars   <- list();
+FM2.crosss   <- list();
+
+FM2.curve    <- NULL;
+FM2.covar    <- NULL;
+FM2.cross    <- NULL;
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 #
@@ -65,8 +75,7 @@ FM2.start <- function()
 #    ret<- FM2.permutation( dat );
 #    summary(ret);
 #    plot(ret);
-#
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+##++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 
 #--------------------------------------------------------------
@@ -99,18 +108,9 @@ FM2.get_value <- function( key, def = NA )
 FM2.get_curve <- function(curve_type)
 {
 	for (i in 1:length(FM2.curves))
-		if (FM2.curves[[i]]$type==curve_type)
+		if (FM2.curves[[i]]$reg_no==curve_type)
 			return(FM2.curves[[i]])
-	
-	return(NULL);
-}
 
-FM2.get_curve_name <- function(curve_name)
-{
-	for (i in 1:length(FM2.curves))
-		if (FM2.curves[[i]]$name==curve_name)
-			return(FM2.curves[[i]])
-	
 	return(NULL);
 }
 
@@ -121,7 +121,7 @@ FM2.reg_curve <- function(curve)
 
 	cat("Curve Registration:", curve$desc, "(", curve$name, ")\n");
 
-	curve$type <-length(FM2.curves)+1; 
+	curve$reg_no<-length(FM2.curves)+1; 
 	for( key in names(class_curve) )
 	{
 		if (!(key %in% names(curve)))
@@ -160,7 +160,7 @@ FM2.reg_cross <- function(cross)
 FM2.get_covar <- function(covar_type)
 {
 	for (i in 1:length(FM2.covars))
-		if (FM2.covars[[i]]$type==covar_type)
+		if (FM2.covars[[i]]$reg_no==covar_type)
 			return(FM2.covars[[i]])
 	
 	return(NULL);
@@ -173,12 +173,11 @@ FM2.reg_covar <- function(covar)
 
 	cat("Covar Registration:", covar$desc, "(", covar$name, ")\n");
 
-	covar$type <-length(FM2.covars)+1; 
+	covar$reg_no <-length(FM2.covars)+1; 
 	FM2.covars[[length(FM2.covars)+1]] <<- covar;
 	
 	return(length(FM2.covars));
 }
-
 
 #--------------------------------------------------------------
 # FM2.param
@@ -193,26 +192,25 @@ FM2.param <- function( par_obj, curve_type, cross_type = CROSS_BC, covar_type=CO
 	curve <- FM2.get_curve(curve_type);
 	if (is.null(curve))
 	{
-		stop("Invalid curve type");
+		stop("inavlid curve type");
 	}
 	FM2.curve <<-curve;
 
 	cross <- FM2.get_cross(cross_type);
 	if (is.null(cross))
 	{
-		stop("Invalid cross type");
+		stop("inavlid cross type");
 	}
 	FM2.cross <<- cross;
-
+	
 	covar <- FM2.get_covar(covar_type);
 	if (is.null(covar))
 	{
-		stop("Invalid covar type");
+		stop("inavlid covar type");
 	}
 	FM2.covar <<- covar;
 
 	par <- dat.get_simuparam( par_obj, cross_type, covar_type );
-
 	class(par)<-"FM2.par";
 	return(par);
 }
@@ -226,11 +224,11 @@ summary.FM2.par <- function( par_obj , file=NA , append = TRUE )
 {
 	if (is.null(FM2.curve))
 	{
-		stop("Invalid curve type");
+		stop("inavlid curve type");
 	}
-
+       
 	str<- dat.summary_par( par_obj );
-
+	
 	if (is.na(file))
 		return ( cat( str ) )
 	else
@@ -246,7 +244,7 @@ FM2.simulate<- function( par_obj )
 {
 	if ( is.null(FM2.curve) )
 	{
-		stop("Invalid curve type");
+		stop("inavlid curve type");
 	}
 
 	dat <- dat.simulate( par_obj );
@@ -260,36 +258,76 @@ FM2.simulate<- function( par_obj )
 # Load a real data, phenotype file, genotype file, marker file 
 # is necessary.
 #--------------------------------------------------------------
-FM2.load_data <- function( pheno_file, geno_file, marker_file, curve_type, cross_type, covar_type=COVAR_AR1  )
+FM2.load_data <- function( pheno_file, geno_file, marker_file  )
 {
 	if (is.null( FM_sys ) )
 		FM2.start();
 
-	curve <- FM2.get_curve(curve_type);
-	if (is.null(curve))
-	{
-		stop("Invalid curve type");
-	}
-	
-	FM2.curve <<- curve;
-
-	cross <- FM2.get_cross(cross_type);
-	if (is.null(cross))
-	{
-		stop("Invalid cross type");
-	}
-	FM2.cross <<- cross;
-
-	covar <- FM2.get_covar(covar_type);
-	if (is.null(covar))
-	{
-		stop("Invalid covar type");
-	}
-	FM2.covar <<- covar;
-
-	dat = dat.load( pheno_file, geno_file, marker_file, cross_type, covar_type );
+	dat <- dat.load( pheno_file, geno_file, marker_file );
 	class(dat)<-"FM2.dat";
 
+	return(dat);
+}
+
+#--------------------------------------------------------------
+# FM2.data_est
+#
+# Load a real data, phenotype file, genotype file, marker file 
+# is necessary.
+#--------------------------------------------------------------
+FM2.data_est <- function( dat,cross_type, curve_type=NA, covar_type=NA, prob=0.05 )
+{
+	if (is.null( FM_sys ) )
+		FM2.start();
+
+	if(class(dat)!="FM2.dat")
+	{
+		stop("inavlid data");
+	}
+	
+	cross.obj <- FM2.get_cross(cross_type);
+	if (is.null(cross.obj))
+	{
+		stop("inavlid cross type");
+	}
+	FM2.cross <<- cross.obj;
+
+	if(is.na(curve_type))
+	{
+		curve_type <- curve.select(dat);
+	}
+	curve.obj <- FM2.get_curve(curve_type);
+	if (is.null(curve.obj))
+	{
+		stop("inavlid curve type");
+	}
+	FM2.curve <<- curve.obj;
+	curve <- FM2.curve$get_est_param(dat, FM2.curve$get_init_rand);
+		if (is.na(curve) || is.na(curve$par) )
+			return(NA);
+	dat$curve <- list(type=FM2.curve$reg_no,  name=FM2.curve$name, par=curve$par, val=curve$val, method=curve$method )
+	
+	if(is.na(covar_type))
+	{
+		covar <- covar.select(dat, curve );
+		covar_type <- covar$reg_no;		
+	}
+	covar.obj <- FM2.get_covar(covar_type);
+	if (is.null(covar.obj))
+	{
+		stop("inavlid covar type");
+	}
+	FM2.covar <<- covar.obj;
+	
+	covar   <- FM2.covar$get_est_param(dat, curve);
+	dat$covar <-list(type=FM2.covar$reg_no, name=FM2.covar$name, par=covar$par, val=covar$val, method=covar$method)
+	
+	
+	dat$curve_name  <- FM2.curve$name;
+	dat$covar_name  <- FM2.covar$name;
+	
+	FM2.set_value("fitting.prob", prob)
+	
 	return(dat);
 }
 
@@ -300,6 +338,11 @@ FM2.load_data <- function( pheno_file, geno_file, marker_file, curve_type, cross
 #--------------------------------------------------------------
 summary.FM2.dat<- function( dat_obj, file=NA , append = TRUE )
 {
+	if (is.null(FM2.curve))
+	{
+		stop("inavlid curve type");
+	}
+
 	str<- dat.summary( dat_obj );
 	if (is.na(file))
 		return ( cat( str ) )
@@ -314,10 +357,15 @@ summary.FM2.dat<- function( dat_obj, file=NA , append = TRUE )
 #--------------------------------------------------------------
 report.FM2.dat<- function( dat_obj )
 {
+	curve <- dat_obj$curve;
+	if (is.null(curve))
+	{
+		stop("inavlid curve type");
+	}
+
 	rprlist<- dat.report( curve, dat_obj );
 	return(rprlist);
 }
-
 
 #--------------------------------------------------------------
 # plot.FM2.dat
@@ -326,9 +374,66 @@ report.FM2.dat<- function( dat_obj )
 #--------------------------------------------------------------
 plot.FM2.dat<- function( dat_obj, plot_type=NA )
 {
+	if (is.null(FM2.curve))
+	{
+		stop("inavlid curve type");
+	}
+
 	dat.plot( dat_obj, plot_type );
 	invisible();
 }
+
+#--------------------------------------------------------------
+# FM2.qtlscan
+#
+# Hypothesis test for data object, the test methods should be 
+# (10,11,12,13,14,15)
+#
+# 1) scan_step, default=2, an interval distance used to scan flanking 
+#    marker, default is 2cm.
+# 2) peak_count, default=5, a number shows how many significant QTL will 
+#    be selected.
+# 3) plot_doctype, default=pdf, the figure output type for summary command.
+#
+#--------------------------------------------------------------
+FM2.qtlmodel<-function( dat_obj, options=list() )
+{
+	if(class(dat_obj)!="FM2.dat")
+	{
+		stop("Invalid data object.");
+	}
+	
+	#scan_step
+	set.scan_step <- FALSE;
+	if (!(is.null(options$scan_step) || is.na(options$scan_step) ))
+	{	
+		set.scan_step 	<- TRUE;
+		old.scan_step   <- FM2.set_value("scan_step", options$scan_step);
+	}
+
+	#peak_count
+	set.peak_count 	<- FALSE;
+	if (!(is.null(options$peak_count) || is.na(options$peak_count) ) )
+	{	
+		set.peak_count 	<- TRUE;
+		old.peak_count  <- FM2.set_value("peak_count", options$peak_count);
+	}
+
+	if (is.null(FM2.curve))
+	{
+		stop("invalid curve type");
+	}
+
+	ret<- FM2.model$qtlscan( dat_obj );
+
+	if (set.scan_step)
+		FM2.set_value("scan_step", old.scan_step);
+	if (set.peak_count)
+		FM2.set_value("peak_count", old.peak_count);
+
+	return (ret);
+}
+
 
 #--------------------------------------------------------------
 # FM2.hp_test
@@ -383,7 +488,7 @@ FM2.qtlmodel<-function( dat_obj, options=list() )
 #
 # Permutation tests.
 #--------------------------------------------------------------
-FM2.permutation<-function( dat_obj, options=list() )
+FM2.permutation<-function( dat_obj, percent, options=list() )
 {
 	old.debug <- NULL;
 	set.debug <- FALSE;
@@ -409,7 +514,8 @@ FM2.permutation<-function( dat_obj, options=list() )
 		set.permu_loop <- TRUE;
 	}
 
-	loops <- as.numeric( FM2.get_value("permu_loop", def=1000) );
+	#loops <- as.numeric( FM2.get_value("permu_loop", def=1000) );
+	loops <- 10;
 	if (loops<100)
 	{
 		#stop("Permutation loop is too few(<100).\n");
@@ -419,10 +525,20 @@ FM2.permutation<-function( dat_obj, options=list() )
 	curve <- dat_obj$curve;
 	if (is.null(FM2.curve))
 	{
-		stop("Invalid curve type");
+		stop("inavlid curve type");
 	}
-
-	ret<- permu.execute( dat_obj );
+	
+	if(percent!=100)
+	{
+		par.full <- getfullpar(dat_obj, 3);
+		dat_obj$par.full <- par.full;
+		pheno <- as.matrix( dat_obj$phenos_table );
+		mat.p3 <- getp(par.full$par, pheno, par.full$w);
+		dat_obj$mat.p3 <- mat.p3;
+	save(dat_obj, file="mat.p3.rdata")
+	}	
+	
+	ret<- permu.execute( dat_obj, percent );
 	class( ret ) <- "FM2.ret.perm";
 
 	if (set.debug)
@@ -431,7 +547,7 @@ FM2.permutation<-function( dat_obj, options=list() )
 		FM2.set_value("cluster_count", old.cluster_count);
 	if (set.permu_loop)
 		FM2.set_value("permu_loop", old.permu_loop);
-
+	
 	return( ret );
 }
 
@@ -496,13 +612,12 @@ plot.FM2.eval.ret<-function( eval_obj )
 	eval.plot( eval_obj );
 }
 
-
 #--------------------------------------------------------------
 # public: FM2.report()
 #               
 #--------------------------------------------------------------
 
-FM2.report<-function( pdf.file, dat, res, perm=NULL, options=list() )
+FM2.report<-function( pdf.file, dat, res, options=list() )
 {
 	Report.new( pdf.file, options );
 	Report.title( "Functional Mappping Report", "FunMap Report", "http://statgen.psu.edu" );
@@ -515,23 +630,16 @@ FM2.report<-function( pdf.file, dat, res, perm=NULL, options=list() )
 	
 	#output the result of curve(1);
 	if ( !is.null(res) )   
-		fre.report_res(dat, res, perm) ;
+		fre.report_res(dat, res) ;
 
 	Report.AddHeadline( "QTL Position", level=1 );
 	
 	#output the result of curve(2);
 	if ( !is.null(res) )	  
-		fre.report_res2(dat, res, perm) ;
-
-	if ( !is.null(perm) )	  
-	{
-		Report.AddHeadline( "Permutation Results", level=1 );
-		fre.report_perm(dat, perm) ;
-	}		
+		fre.report_res2(dat, res) ;
 
 	Report.Output( pdf.file );
 }
-
 
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Function: FM2.one_step
@@ -561,47 +669,60 @@ FM2.report<-function( pdf.file, dat, res, perm=NULL, options=list() )
 #    and count of parameters for composite model.
 #
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-FM2.qtlscan<-function(pheno_csv, geno_csv, marker_csv, curve_type, cross_type, covar_type=COVAR_AR1, options=list())
+FM2.qtlscan<-function(pheno_csv, geno_csv, marker_csv, curve_type, cross_type, covar_type, options=list(prob=0.05))
 {
 	if (is.null( FM_sys ) )
 		FM2.start();
 
-	#load data
-	dat_os<- FM2.load_data( pheno_csv, geno_csv, marker_csv, curve_type, cross_type, covar_type );
+	dat_os<- FM2.load_data( pheno_csv, geno_csv, marker_csv )
 	if (is.null(dat_os))
-		stop("STOP:Failed to load the phenotype, genotype and marker file.\n");
+		stop("STOP:Failed to load the phenotype.\n");
 	
-	file.summary<-"";
+	est <- FM2.data_est( dat_os, curve_type, cross_type, covar_type, options$prob );
+	if (is.na(est) || is.null(est))
+		cat("Failed to estimate the data.\n")
+	else
+	{
+		dat_os$covar <- est$covar;
+		dat_os$curve <- est$curve;
+	}
+
+	file.summary<-""
 	if (!is.null(options$file.summary))
 		file.summary <- options$file.summary;
 	cat("\nSummary report:\n", file=file.summary);
 	
 	try( summary( dat_os, file=file.summary ) );
 
-	ret_os<- FM2.qtlmodel( dat_os, options=options );
-
+	ret_os<- FM2.qtlscan( dat_os, options=options );
+ 	summary(ret_os, dat_os);
+ 	
 	##Save 1
 	if (is.null( options$file.rdata) )
-		options$file.rdata = paste(pheno_csv, ".rdata", sep="")
-	save(dat_os, ret_os, file=options$file.rdata );
+		save(dat_os, ret_os, file=paste(pheno_csv, ".rdata", sep="") )
+	else
+		save(dat_os, ret_os, file=options$file.rdata );
 
 	#report
 	if (is.null( options$file.report))
-		options$file.report <- paste(pheno_csv, ".pdf", sep="");
-	FM2.report( options$file.report, dat_os, ret_os );
+	{
+		file.rpt <- paste(pheno_csv, ".pdf", sep="");
+		FM2.report( file.rpt, dat_os, ret_os);
+	}
+	else
+		FM2.report( options$file.report, dat_os, ret_os )
 	
 	nLoop <- FM2.get_value("permu_loop", def=1000);
 	if ( !is.null(options$permu_loop) && !is.na(options$permu_loop) )
 		nLoop <- as.numeric( options$permu_loop );
 
-	ret_perm_os <- NULL;
 	if (nLoop>0)
 	{
 		ret_perm_os<- FM2.permutation( dat_os, options=options);
 		summary( ret_perm_os, file=file.summary );
 
 		p05 <- 0;
-		p05 <- 0;
+		p01 <- 0;
 		idx05 <- which(ret_perm_os$pv_table[,1]==0.05);
 		idx01 <- which(ret_perm_os$pv_table[,1]==0.01);
 		if (length(idx05)>0 )
@@ -622,11 +743,6 @@ FM2.qtlscan<-function(pheno_csv, geno_csv, marker_csv, curve_type, cross_type, c
 	}
 	else
 		try( summary(ret_os, dat_os, file=file.summary) );
-	
-	#report
-	if (is.null( options$file.report))
-		options$file.report <- paste(pheno_csv, ".pdf", sep="");
-	FM2.report( options$file.report, dat_os, ret_os, ret_perm_os )
 
 	return(ret_os);
 }
@@ -639,39 +755,67 @@ FM2.qtlscan<-function(pheno_csv, geno_csv, marker_csv, curve_type, cross_type, c
 # 
 # The result is saved into [LC_simu_test_XX_XX.rdata]
 #++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-FM2.simu_test<-function( par_obj, curve_type, cross_type, covar_type= COVAR_AR1 )
+
+FM2.simu_test<-function(cross_type, par_obj=par_LC, curve_type=CURVE_LC, covar_type =COVAR_AR1)
 {
 	if (is.null( FM_sys ) )
 		FM2.start();
-
-	par_st<- FM2.param( par_obj, curve_type, cross_type, covar_type );
+		
+	par_st<- FM2.param( par_obj, curve_type, cross_type , covar_type);
 	summary(par_st);
 	
 	dat_st<- FM2.simulate( par_st);
-	summary(dat_st);
-
-	ret_st<- FM2.qtlmodel( dat_st);
-	summary(ret_st, dat_st);
-
-	##Step 1
-	rdata.name <- paste( "simu_test_",FM2.curve$name,"_",FM2.cross$name,".rdata", sep="");
-	save(dat_st, ret_st, file = rdata.name );
-
-	##Step 2(Report) 
-	pdf.name <- paste( "simu_test_",FM2.curve$name,"_",FM2.cross$name,".pdf", sep="");
-	FM2.report(pdf.name, dat_st, ret_st);
-
-	##Step 3(Ppermutation) 
-	FM2.set_value("debug", TRUE)
-	ret_perm_st<- FM2.permutation( dat_st, options=list(permu_loop=100) );
-	summary( ret_perm_st );
-	try(plot(ret_perm_st));
-
-	FM2.report(pdf.name, dat_st, ret_st, ret_perm_st);
 	
-	##Step 4(Save All Data)
-	save(dat_st, ret_st, ret_perm_st, file = rdata.name );
-	cat("\nNotice: The results(dat_st, ret_st and ret_perm_st) are stored into ", rdata.name, "\n");
+	if (is.null(dat_st))
+		stop("STOP:Failed to load the phenotype.\n");
+	
+	curve <- FM2.curve$get_est_param(dat_st, FM2.curve$get_init_rand);
+		#if (is.na(curve) || is.na(curve$par) )
+		#return(NA);
+	
+	est <- FM2.data_est(dat_st, cross_type, curve_type, covar_type );
+	if (is.null(est))
+		stop("STOP:Failed to estimate the data.\n");
 
-	return( list(dat_st, ret_st, ret_perm_st) );
+	dat_st$covar <- est$covar;
+	dat_st$curve <- est$curve;
+        
+	summary(dat_st);
+       	
+       	library("parallel")
+	#r.permu.20  <- permutation(dat_st, 20)
+       	
+       	percent=c(100, 20, 10, 5)
+	
+	par.full <- getfullpar(dat_st, 3);
+	dat_st$par.full <- par.full;
+	pheno <- as.matrix( dat_st$phenos_table );
+	mat.p3 <- getp(par.full$par, pheno, par.full$w);
+	dat_st$mat.p3 <- mat.p3;
+save(dat_st, file = "mat.p3.rdata")
+	
+	ret_st<- FM2.qtlmodel( dat_st);
+	
+	fun <- function(i.loop){ 
+	cat("PERMUTATION:LOOP", i.loop, "is starting ....\n" )
+	
+		permu.execute(i.loop, dat_st)
+	}
+	
+       	nLoop <- 1000;
+       
+	Ret <- mclapply( 1:nLoop, fun, mc.cores=15);
+	
+	perm.dat <- fin.permu(Ret, percent, nLoop)
+	
+	for(i in 1:5)
+	{
+cat("the mathod", i, "\n")
+
+	fpt.rank.perm(perm.dat$ret_perm[[i]], i)
+	fpt.plot_fil.permu(perm.dat$pv_table[[i]], i)
+	}
+	
+	save(dat_st, ret_st, Ret, file="Ret.rdata")
+	
 }
--- Funmap2/R/zzz.r
+++ Funmap2/R/zzz.r
@@ -1,28 +1,26 @@
-msg <- function(...)
+.onAttach<- function(libname, pkgName)
 {
     date <- date();
     x <- regexpr("[0-9]{4}", date);
     yr <- substr(date, x[1], x[1] + attr(x, "match.length") - 1);
  
-    cat("||\n");
-    cat("|| Funmap2 Package v.2.5\n");
-    cat("|| Build date: ", date(), "\n");
-    cat("|| Copyright (C) 2011-", yr, ", http://statgen.psu.edu\n", sep="");
-    cat("|| Written by Zhong Wang(wzhy2000@hotmail.com)\n");
-    cat("||\n");
+    packageStartupMessage("||");
+    packageStartupMessage("|| Funmap2 Package v.2.5");
+    packageStartupMessage("|| Build date: ", date(), "");
+    packageStartupMessage("|| Copyright (C) 2011-", yr, ", http://statgen.psu.edu", sep="");
+    packageStartupMessage("|| Written by Zhong Wang(wzhy2000@hotmail.com)");
+    packageStartupMessage("||");
 }
 
-.onAttach<- function(libname, pkgName)
+.onLoad<- function(libname, pkgName)
 {
-	msg();
-
-FM_sys <<- NULL;
-FM2.curves   <<- list();
-FM2.covars   <<- list();
-FM2.crosss   <<- list();
-FM2.curve    <<- NULL;
-FM2.covar    <<- NULL;
-FM2.cross    <<- NULL;
+	FM_sys <<- NULL;
+	FM2.curves   <<- list();
+	FM2.covars   <<- list();
+	FM2.crosss   <<- list();
+	FM2.curve    <<- NULL;
+	FM2.covar    <<- NULL;
+	FM2.cross    <<- NULL;
 
 	FM2.start();
 	ZZZ.regcovar();
